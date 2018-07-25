58d19b4a252f1cbfb11ff3953947c4ffb992e10e
Preston Lim <prestonlimlianjie@gmail.com>
2018-07-25 23:56:51 +0800

    removed _site folder

--- _site/assets/js/sideNav-offset.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    $('.sidenav').stickySidebar({
-        topSpacing: 40,
-        bottomSpacing: 40,
-        containerSelector: '.has-side-nav',
-        innerWrapperSelector: '.sidebar__inner'
-    });
-});
